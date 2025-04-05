57371a258df7860a6d464f0a420f0b3ff81a0828
Surya Darma <budisuryadarma0407@gmail.com>
2025-04-05 22:59:32 +0800

    adding firs page

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,16 @@ import heroImageBig from './assets/preweds/hero-image-big.jpg';
 const Page = () => {
   return (
     <>
-      <section className="relative min-h-screen w-full bg-[#FFF1D5]">
-        <div className="mx-auto w-full max-w-[430px] min-h-screen bg-primary relative overflow-hidden border-x border-gray-200">
+      <section className="relative min-h-screen w-full bg-testpink bg-[#FFF1D5]">
+        <div className="mx-auto w-full max-w-[430px] min-h-screen bg-primary relative overflow-hidden">
           <div className="w-full h-full bg-red-50 relative">
             {/* <ImageSlideshow /> */}
             <div
               className="h-screen w-full bg-cover bg-[center_bottom_0px] absolute"
-              style={{backgroundImage: `url(${heroImageBig})`, backgroundPositionY: '-150px'}}
+              style={{
+                backgroundImage: `url(${heroImageBig})`,
+                backgroundPositionY: '-150px',
+              }}
             ></div>
             <div
               className="h-screen w-full absolute top-0 left-0 z-10 bg-black opacity-30"
@@ -21,25 +24,36 @@ const Page = () => {
               //   background: `linear-gradient(0deg, rgba(0, 0, 0, 0.30) 1%, rgba(0, 0, 0, 0.30) 100%), linear-gradient(180deg, rgba(0, 0, 0, 0.00) 0%, rgba(0, 0, 0, 0.80) 70.16%)`
               // }}
             ></div>
-            <div className="flex justify-center flex-col items-center h-screen w-full max-w-[430px] mx-auto relative z-20" style={{
-              background: 'linear-gradient(to bottom, rgba(255, 241, 213, 0) 0%, rgba(255, 241, 213, 0.3) 70%, rgba(255, 241, 213, 1) 80%)'
-            }}>
-              <h1 className="flex flex-col items-center text-white .pt-serif-regular">
-                <span className="block text-2xl">The Wedding of</span>
-                <span className="block text-xl">Apri & Surya</span>
-              </h1>
-              <p className="text-lg text-white text-center">
-                <span className="block">Kepada Yth.</span>
-                <span className="block">Bapak/ Ibu/ Saudara/ i</span>
-              </p>
-              <p className="pt-serif-regular-italic">John Doe</p>{' '}
+            <div
+              className="h-screen w-full max-w-[430px] mx-auto relative z-20"
+              style={{
+                background:
+                  'linear-gradient(to bottom, rgba(255, 241, 213, 0) 0%, rgba(255, 241, 213, 0.3) 70%, rgba(255, 241, 213, 1) 80%)',
+              }}
+            >
+              <div className='absolute top-0 bottom-0 left-0 right-0 items-center justify-center flex'>
+                <div className='h-2/3 w-3/4 rounded-full border-[#FFF1D5] border-double border-[5px]' />
+              </div>
+              <div className="absolute bottom-[75px] right-0 left-0 flex flex-col justify-center items-center z-10">
+                <h1 className="flex flex-col items-center text-[#4B3B2A] text-sm tracking-[2px] font-normal">
+                  <span>THE WEDDING OF</span>
+                </h1>
+                <p className="block text-4xl text-[#4B3B2A] pt-cormorant">
+                  Apri & Surya
+                </p>
+                <p className="text-[#4B3B2A] text-center text-sm tracking-[2px] font-normal">
+                  <span className="block">Kepada Yth.</span>
+                  <span className="block">Bapak/ Ibu/ Saudara/ i</span>
+                </p>
+                <p className="pt-cormorant text-sm font-normal bg-white p-3 rounded-sm mt-4">Justin Bieber</p>{' '}
+              </div>
             </div>
           </div>
         </div>
       </section>
       <section className="w-full">
         <div className="w-full max-w-[430px] border-double border-[2px] mx-auto">
-          <span className='font-'>
+          <span className=".pt-cormorant">
             Ihaiva stam ma vi yaustam, visvam ayur vyasnutam, kridantau putrair
             naptrbhih, modamanau sve grhe.
           </span>
