084bc5a38fa16cdc4d7f82b16170c3222bb4f6ed
Surya Darma <budisuryadarma0407@gmail.com>
2025-04-15 23:39:09 +0800

    Updates

---
+++ assets/EventCard-BQ1czrQV.js
@@ -0,0 +1,11 @@
+import{c as l,j as e}from"./index-By9k8Mn-.js";/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const s=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["polyline",{points:"12 6 12 12 16 14",key:"68esgv"}]],a=l("clock",s);/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0",key:"1r0f0z"}],["circle",{cx:"12",cy:"10",r:"3",key:"ilqhr7"}]],n=l("map-pin",c),x=t=>e.jsxs("div",{className:"rounded-4xl w-full min-h-[350px] relative overflow-hidden flex",children:[e.jsx("div",{className:"w-full h-full absolute bg-cover",style:{backgroundImage:`url(${t.bgImage})`}}),e.jsx("div",{className:"h-full w-full absolute",style:{background:"rgb(73 66 54 / 78%) 0%"}}),e.jsxs("div",{className:"relative flex flex-1 flex-col items-center px-3 py-5 z-10 font-poppins",children:[e.jsx("h4",{className:"font-poppins text-2xl font-bold text-goldy mb-2",children:t.title}),e.jsxs("div",{className:"[&>*:not(:last-child)]:mb-2 flex flex-1 flex-col items-center justify-evenly",children:[e.jsx("span",{className:"text-white text-lg",children:t.dayName}),e.jsx("span",{className:"text-white text-lg",children:t.date}),e.jsxs("div",{className:"flex flex-row text-sm items-center",children:[e.jsx(a,{className:"text-white mr-2",width:16,height:16}),e.jsx("span",{className:"text-white text-sm",children:t.startAndEnd})]}),e.jsx("span",{className:"text-white text-center text-sm",children:t.location}),e.jsxs("a",{href:t.gMap,className:"flex flex-row items-center bg-white p-2 rounded-sm text-xs font-poppins",target:"_blank",children:[e.jsx(n,{className:"mr-2",width:16,height:16}),e.jsx("span",{children:"Google Map"})]})]})]})]});export{x as default};
