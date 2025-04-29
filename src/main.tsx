1130d560fd9c6a16720cfb70bbcfb1a0af477ab6
Surya Darma <budisuryadarma0407@gmail.com>
2025-04-29 10:59:30 +0800

    move css to  html

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import MediaContextProvider from './components/MediaContext/Provider.tsx';
 
-import './index.css';
 import 'react-loading-skeleton/dist/skeleton.css';
 
 createRoot(document.getElementById('root')!).render(
