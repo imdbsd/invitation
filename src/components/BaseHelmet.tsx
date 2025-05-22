dfceba70719a69c6c134f481047f44a567270abc
Surya Darma <budisuryadarma0407@gmail.com>
2025-05-22 15:45:58 +0800

    separated og_image

--- src/components/BaseHelmet.tsx
+++ src/components/BaseHelmet.tsx
@@ -17,19 +17,19 @@ export const WEDDING_META: OGMeta = {
   description:
     'Celebrating the love of Surya & Apri — 11 Juni 2025. View their story and wedding moments',
   image: {
-    src: 'https://raw.githubusercontent.com/imdbsd/invitation/refs/heads/main/public/og_image_prewed.jpg',
+    src: `${import.meta.env.BASE_URL}/og_image_prewed.jpg`,
     width: '600',
     height: '325',
   },
 };
 
 export const SIRAMAN_META: OGMeta = {
-  title: 'Surya & Apri Tied The Knot at 1 Juni 2025',
+  title: 'Siraman Surya & Apri Tied The Knot at 1 Juni 2025',
   url: 'https://suryaapritiedtheknot.site/siraman',
   description:
     'Celebrating the love of Surya & Apri — 1 Juni 2025. View their story and wedding moments',
   image: {
-    src: 'https://raw.githubusercontent.com/imdbsd/invitation/refs/heads/main/public/og_image_prewed.jpg',
+    src: `${import.meta.env.BASE_URL}/og_image_siraman.jpg`,
     width: '600',
     height: '325',
   },
