7de4f8725a02d30382dcb813b1984f638b6776f6
Surya Darma <budisuryadarma0407@gmail.com>
2025-06-03 17:07:11 +0800

    unique track key

--- src/components/TrackProvider/Provider.tsx
+++ src/components/TrackProvider/Provider.tsx
@@ -1,22 +1,26 @@
 import * as React from 'react';
 import {v4 as uuidv4} from 'uuid';
 import {TrackContext} from './Context';
+import useSearchParams from '../../hooks/useSearchParams';
 
 const TRACK_ID_KEY = '2v4TCobBC3';
 
 export const TrackProvider = (props: React.PropsWithChildren) => {
+  const params = useSearchParams<'to'>();
   const uuid: string | null = React.useMemo(() => {
     if (typeof window === 'undefined') return null;
 
-    const storedUUID = window.localStorage.getItem(TRACK_ID_KEY);
+    const key = `${TRACK_ID_KEY}${params?.to ? `-${params.to}` : ''}`;
+
+    const storedUUID = window.localStorage.getItem(key);
     if (storedUUID) return storedUUID;
 
     const generatedUUID = uuidv4();
 
-    window.localStorage.setItem(TRACK_ID_KEY, generatedUUID);
+    window.localStorage.setItem(key, generatedUUID);
 
     return generatedUUID;
-  }, []);
+  }, [params]);
 
   return (
     <TrackContext.Provider value={{uuid}}>
